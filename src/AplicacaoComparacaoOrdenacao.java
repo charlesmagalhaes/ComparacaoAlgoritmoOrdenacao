a593ff182e1321f290b4ca003f9a4f589f92b97c
Charles Magalhaes <charlesrm29@gmail.com>
2022-10-25 15:08:32 -0300

    Execução dos métodos na classe principal

--- src/AplicacaoComparacaoOrdenacao.java
+++ src/AplicacaoComparacaoOrdenacao.java
@@ -5,22 +5,100 @@ public class AplicacaoComparacaoOrdenacao {
 	public static void main(String[] args) {
 		
 		AlgoritmoOrdenacao ordenacao = new AlgoritmoOrdenacao();
+		int[] listaGerada;
 		
-		Random gerador = new Random();
+		long tempoInicial;
+		long somaInsertion = 0 ;
+		long somaBubble = 0;
+		long somaMerge = 0;
 		
-		int[] listaGerada = ordenacao.gerarArrayRandom(1, 10000, 500);
+		for(int i=0; i< 5;i++) {
+	
+			listaGerada = ordenacao.gerarArrayRandom(1, 10000, 10000);
+			
+			 tempoInicial = System.currentTimeMillis();
+			 ordenacao.insertionSort(listaGerada.clone());
+			 somaInsertion += System.currentTimeMillis() - tempoInicial;
+		     
+		     tempoInicial = System.currentTimeMillis();
+			 ordenacao.bubbleSort(listaGerada);
+			 somaBubble += System.currentTimeMillis() - tempoInicial;
+		     
+		     int[] listaTeste = listaGerada.clone();
+		     tempoInicial = System.currentTimeMillis();
+			 ordenacao.sort(listaTeste, 0, listaTeste.length-1);
+			 somaMerge += System.currentTimeMillis() - tempoInicial;
+			
+			
+		}
 		
-		AlgoritmoOrdenacao.imprimirArray(listaGerada, "Lista Gerada: ");
+		System.out.println("--------------Dados de entrada aleatório-------------------\n");
+		System.out.println("Insertion sort time media: "+ somaInsertion/5);
+		System.out.println("Bubble sort time media: "+ somaBubble/5);
+		System.out.println("Merge sort time media: "+ somaMerge/5+"\n");
 		
-		ordenacao.insertionSort(listaGerada);
 		
 		
-		AlgoritmoOrdenacao.imprimirArray(listaGerada, "Lista Gerada insert sort: ");
 		
-		ordenacao.insertionSortDesc(listaGerada);
+		for(int i=0; i< 5;i++) {
+			
+			listaGerada = ordenacao.gerarArrayRandom(1, 10000, 10000);
+			ordenacao.insertionSort(listaGerada);
 		
-		AlgoritmoOrdenacao.imprimirArray(listaGerada, "Lista Gerada insert sort desc: ");
+			 tempoInicial = System.currentTimeMillis();
+			 ordenacao.insertionSort(listaGerada.clone());
+			 somaInsertion += System.currentTimeMillis() - tempoInicial;
+		     
+		     tempoInicial = System.currentTimeMillis();
+			 ordenacao.bubbleSort(listaGerada);
+			 somaBubble += System.currentTimeMillis() - tempoInicial;
+		     
+		     int[] listaTeste = listaGerada.clone();
+		     tempoInicial = System.currentTimeMillis();
+			 ordenacao.sort(listaTeste, 0, listaTeste.length-1);
+			 somaMerge += System.currentTimeMillis() - tempoInicial;
+			
+			
+		}
+		
+		System.out.println("--------------Dados de entrada já ordenados-------------------\n");
+		System.out.println("Insertion sort time media: "+ somaInsertion/5);
+		System.out.println("Bubble sort time media: "+ somaBubble/5);
+		System.out.println("Merge sort time media: "+ somaMerge/5);
+		
+		
+		
+		
+		for(int i=0; i< 5;i++) {
+			
+			listaGerada = ordenacao.gerarArrayRandom(1, 10000, 10000);
+			ordenacao.insertionSortDesc(listaGerada);
+		
+			 tempoInicial = System.currentTimeMillis();
+			 ordenacao.insertionSort(listaGerada.clone());
+			 somaInsertion += System.currentTimeMillis() - tempoInicial;
+		     
+		     tempoInicial = System.currentTimeMillis();
+			 ordenacao.bubbleSort(listaGerada);
+			 somaBubble += System.currentTimeMillis() - tempoInicial;
+		     
+		     int[] listaTeste = listaGerada.clone();
+		     tempoInicial = System.currentTimeMillis();
+			 ordenacao.sort(listaTeste, 0, listaTeste.length-1);
+			 somaMerge += System.currentTimeMillis() - tempoInicial;
+			
+			
+		}
+		
+		System.out.println("--------------Dados de entrada ordenados de forma decrescente-------------------\n");
+		System.out.println("Insertion sort time media: "+ somaInsertion/5);
+		System.out.println("Bubble sort time media: "+ somaBubble/5);
+		System.out.println("Merge sort time media: "+ somaMerge/5);
+		
 		
 	}
+	
+	
+	
 
 }
